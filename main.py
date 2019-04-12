Implemented gradient checking. Gradients are not equal. Something is wrong with calculating gradient in second layer

main.py
@@ -6,6 +6,7 @@ import visualize
 import time
 import matplotlib.pyplot as plt
 
+
 def main():
     #load train and test data
     print('Loading test and train data from "%s".' % DATASETS_PATH)
@@ -15,6 +16,7 @@ def main():
     x_train = x_train[:]
     y_train = y_train[:]
 
+
     #display it to check if everything is ok
     print('Visualizing first 25 examples')
     visualize.display_data(x_train, 5, 5)
@@ -23,19 +25,21 @@ def main():
     #1 pick a network architecture
     number_of_inputs = 28 * 28 #dimensions of the images
     number_of_outputs = 10 #10 classes - digits from 0 to 9
-    number_of_hidden_units = 10 * 10 #number of hidden units in 1'st (and, for now, only) layer
+    number_of_hidden_units = 10 #number of hidden units in 1'st (and, for now, only) layer
     epsilon = sqrt(6)/sqrt(number_of_inputs + number_of_outputs) #epsilon used to initialize weights in NN. Every connection will be randomly initialized by a number from range [-epsilon, epsilon]
     
     print("Creating a NN with:\n{} input units\n{} output units\n{} hidden layers with {} hidden units each (not including a bias unit)\nepsilon used to initialize weights: {}".format(number_of_inputs, number_of_outputs, 1, number_of_hidden_units, epsilon))
     #Neural Network used to train data
-    nn1 = OneLayerNeuralNetwork(num_inputs = number_of_inputs, num_outputs = number_of_outputs, num_hidden = number_of_hidden_units, epsilon = epsilon, random_seed = 1)
-    nn2 = MultiLayerNeuralNetwork([number_of_inputs, number_of_hidden_units, number_of_outputs], epsilon, 1)
+    #nn1 = OneLayerNeuralNetwork(num_inputs = number_of_inputs, num_outputs = number_of_outputs, num_hidden = number_of_hidden_units, epsilon = epsilon, random_seed = 0)
+    nn1 = MultiLayerNeuralNetwork([number_of_inputs, number_of_hidden_units, number_of_hidden_units, number_of_outputs], epsilon = epsilon, random_seed = 0)
     
     #transforming each training and test example to 1D
     x_train = x_train.reshape((x_train.shape[0], x_train.shape[1] * x_train.shape[2]))
     x_test = x_test.reshape((x_test.shape[0], x_test.shape[1] * x_test.shape[2]))
 
-    
+    #x_train = x_train / 255 # "feature scaling"
+    #x_test = x_test / 255 # "feature scaling"
+
     #transforming each label to a vector of 0's, where vector[label] = 1
     #TODO: vectorize the loop
     tmp = np.zeros((y_train.size, number_of_outputs))
@@ -43,34 +47,24 @@ def main():
         tmp[i, int(y_train[i])] = 1
     y_train = tmp
     
+    if True:
+        print("Gradient checking\nApprox | Calculated by NN")
+        approx, grad = nn1.gradient_checking(x_train[0:100], y_train[0:100], 0.1)
+        for i in range(approx.size):
+            a = approx[i]
+            g = grad[i]
+            rd = a - g
+            print("{:5f}\t{:5f}\t{}".format(approx[i], grad[i], rd))
     print("Shape of training set: {}x{}\nShape of training labels: {}x{}".format(*x_train.shape, *y_train.shape))
     #training neural network
     start = time.perf_counter()
-    params = (x_train, y_train, 10, 0.1, 0.1)
+    params = (x_train, y_train, 20, 0.3, 1)
     nn1.train(*params)
-    nn2.train(*params)
     print("Trained NN in %fs." % (time.perf_counter() - start))
 
     #make predictions
-    predictions1 = nn1.predict(x_test)
-    predictions1 = np.argmax(predictions1, 1)
-    predictions2 = nn2.predict(x_test)
-    predictions2 = np.argmax(predictions2, 1)
-    m = predictions1.size
-    hits = [0, 0]
-    misses = [0, 0]
-    for t in range(m):
-        if predictions1[t] == y_test[t]:
-            hits[0] += 1
-        else:
-            misses[0] += 1
-        if predictions2[t] == y_test[t]:
-            hits[1] += 1
-        else:
-            misses[1] += 1
-    print("OneLayerNN:'n{} hits, {} misses. {}% accuracy.\n".format(hits[0], misses[0], hits[0] / m * 100))
-    print("MultiLayerNN:'n{} hits, {} misses. {}% accuracy.\n".format(hits[1], misses[1], hits[1] / m * 100))
-        
+    hits, misses = nn1.make_predictions(x_test, y_test)
+    print("OneLayerNN:\n{} hits, {} misses. {}% accuracy.\n".format(hits, misses, hits / (hits + misses) * 100))    
 
 
     #plot cost for each iteration

neural_network.py
@@ -70,13 +70,15 @@ class OneLayerNeuralNetwork:
         a3 = self.sigmoid(z3)
         return a1, z2, a2, z3, a3
 
-    def train(self, X, y, epochs, learning_rate = 0.1, regularization_parameter = 1):
-        for i in range(epochs):
+    def train(self, X, y, epochs, learning_rate = 0.1, regularization_parameter = 0.1):
+        last_cost = 0
+        for i in range(1, epochs + 1):
             theta1_grad, theta2_grad = self.theta_grad(X, y, regularization_parameter)
             self.theta1 -= learning_rate * theta1_grad
             self.theta2 -= learning_rate * theta2_grad
             self.errors.append(self.cost_function(X, y, regularization_parameter))
-            print("{}'th iteration. Cost: {}".format(i + 1, self.errors[-1]))
+            print("{}'th iteration. Cost: {:f} ({:f}).".format(i, self.errors[-1], self.errors[-1] - last_cost))
+            last_cost = self.errors[-1]
         
     def predict(self, X):
         '''
@@ -84,6 +86,19 @@ class OneLayerNeuralNetwork:
         '''
         *_, h_x = self.forward_propagation(X)
         return h_x
+
+    def make_predictions(self, X, y):
+        predictions = self.predict(X)
+        predictions = np.argmax(predictions, 1)
+        m = predictions.size
+        hits = 0
+        misses = 0
+        for t in range(m):
+            if predictions[t] == y[t]:
+                hits += 1
+            else:
+                misses += 1
+        return hits, misses
 
     def gradient_approx(self):
         pass
@@ -96,69 +111,53 @@ class OneLayerNeuralNetwork:
 
 
 class MultiLayerNeuralNetwork:
-    '''
-    One layer Neural Network
-    '''
 
-    def __init__(self, layer_sizes, epsilon, random_seed = None):
+    def __init__(self, layers, epsilon, random_seed = None):
         state = np.random.RandomState(random_seed)
-        self.thetas = []
-        for i in range(1, len(layer_sizes)):
-            self.thetas.append(state.rand(layer_sizes[i], layer_sizes[i-1] + 1) * 2 * epsilon - epsilon)
-        '''
-        #matrix of weights, used to calculate activation values in layer 2 in respect to values in layer 1 (input)
-        self.theta1 = np.random.rand(num_hidden, num_inputs + 1) * 2 * epsilon - epsilon
-        #matrix of weights used to calculate activation values on layer 3 (output layer)
-        self.theta2 = np.random.rand(num_outputs, num_hidden + 1) * 2 * epsilon - epsilon
-        '''
+        self.theta_vector = state.rand(sum((l + 1) * nl for l, nl in zip(layers, layers[1:])), 1) * 2 * epsilon - epsilon
+        self.thetas = self.thete_vector_to_list(self.theta_vector, layers)
         self.errors = []
+        self.layers = layers
+
+    
+    def thete_vector_to_list(self, theta_vector, layers):
+        i = 0
+        thetas = []
+        for l, nl in zip(layers, layers[1:]):
+            l += 1
+            i_e = i + l * nl
+            theta = theta_vector[i:i_e].reshape((nl, l))
+            thetas.append(theta)
+            i = i_e
+        return thetas
 
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
 
-    def cost_function(self, X, y, regularization_parameter):
+    def cost_function(self, X, y, regularization_parameter, theta_vector = None, thetas = None):
         '''
         Cost function for logistic regression:
             J(theta) = 1/m * [sum(i = 1...m)[sum(k = 1...K)[-y_ik * log(h_x_i)_k - (1-y_ik) * log(1 - h_x_i)_k]]]
         '''
+        if theta_vector is None:
+            theta_vector = self.theta_vector
+        if thetas is None:
+            thetas = self.thetas
+        
         # make y a m x k matrix (translate each label to vector)
         m = X.shape[0]
-        *_, h_x = self.forward_propagation(X)
-        return -1/m * np.sum(
+        h_x = self.predict(X, thetas)
+        c1 = -1/m * np.sum(np.sum(
                     y * np.log(h_x) + (1-y) * np.log(1 - h_x)
-                ) + regularization_parameter / (2 * m) * np.sum((np.sum(theta**2) for theta in self.thetas))
+                ))
+        c2 = regularization_parameter / (2 * m) * np.sum(np.sum(theta_vector ** 2))
+        return c1 + c2
 
-    def theta_grad(self, X, y, regularization_parameter):
+    def theta_grad(self, X, y, regularization_parameter, thetas = None):
+        if thetas is None:
+            thetas = self.thetas
         m = y.shape[0] #number of examples
-        '''
-        D1 = np.zeros(self.theta1.shape)
-        D2 = np.zeros(self.theta2.shape)
-        
-        #forward propagation for each example
-        A1, Z2, A2, Z3, A3 = self.forward_propagation(X)
-
-        #backward propagation for each example
-        for t in range(m):
-            #column vector by convention
-            x = X[[t]].T
-            a3 = A3[[t]].T
-            a2 = A2[[t]].T
-            a1 = A1[[t]].T #just x, but with added bias term
-            yt = y[[t]].T
-            delta3 = a3 - yt
-            delta2 = self.theta2.T @ delta3 * a2 * (1 - a2)
-            delta2 = delta2[1:,:] #remove delta2_0
-            D1 += delta2 @ a1.T
-            D2 += delta3 @ a2.T
-        theta1_grad = D1/m
-        theta2_grad = D2/m
-        #now add regularization part
-        theta1_grad[:, 1:] += regularization_parameter / m * self.theta1[:, 1:]
-        theta2_grad[:, 1:] += regularization_parameter / m * self.theta2[:, 1:]
-
-        return theta1_grad, theta2_grad
-        '''
-        D = [np.zeros(t.shape) for t in self.thetas]
+        D = [np.zeros(t.shape) for t in thetas]
         AZ = self.forward_propagation(X)
         for t in range(m):
             A = AZ[::2]
@@ -166,31 +165,24 @@ class MultiLayerNeuralNetwork:
             yt = y[[t]].T
             
             deltas = [A[-1] - yt]
-            for theta, a in zip(self.thetas[::-1], A[-2:0:-1]):
+            for theta, a in zip(thetas[::-1], A[-2:0:-1]):
                 delta = theta.T @ deltas[-1] * a * (1 - a)
                 delta = delta[1:,:]
                 deltas.append(delta)
             for d, delta, a in zip(D, deltas[::-1], A):
                 d += delta @ a.T
         theta_grads = [d / m for d in D]
-        for theta_grad, theta in zip(theta_grads, self.thetas):
+        for theta_grad, theta in zip(theta_grads, thetas):
             theta_grad[:, 1:] += regularization_parameter / m * theta[:, 1:]
         return theta_grads
 
-    def forward_propagation(self, X):
+    def forward_propagation(self, X, thetas = None):
         m = X.shape[0]
-        '''
-        a1 = np.concatenate([np.ones((m, 1)), X], 1) # m x (n + 1), where n is number of features
-        z2 = a1 @ self.theta1.T #theta1 is l_1 x (n + 1), result is m x l_1
-        a2 = self.sigmoid(z2)
-        a2 = np.concatenate([np.ones((m, 1)), a2], 1) # m x (l_1 + 1)
-        z3 = a2 @ self.theta2.T #theta2 is k x (l_1 + 1), result is m x k
-        a3 = self.sigmoid(z3)
-        return a1, z2, a2, z3, a3
-        '''
+        if thetas is None:
+            thetas = self.thetas
         a = np.concatenate([np.ones((m, 1)), X], 1)
         ret = [a]
-        for theta in self.thetas:
+        for theta in thetas:
             z = a @ theta.T
             a = self.sigmoid(z)
             a = np.concatenate([np.ones((m, 1)), a], 1)
@@ -202,14 +194,6 @@ class MultiLayerNeuralNetwork:
         return ret
 
     def train(self, X, y, epochs, learning_rate = 0.1, regularization_parameter = 1):
-        '''
-        for i in range(epochs):
-            theta1_grad, theta2_grad = self.theta_grad(X, y, regularization_parameter)
-            self.theta1 -= learning_rate * theta1_grad
-            self.theta2 -= learning_rate * theta2_grad
-            self.errors.append(self.cost_function(X, y, regularization_parameter))
-            print("{}'th iteration. Cost: {}".format(i + 1, self.errors[-1]))
-        '''
         for i in range(epochs):
             theta_grads = self.theta_grad(X, y, regularization_parameter)
             for theta, theta_grad in zip(self.thetas, theta_grads):
@@ -217,20 +201,47 @@ class MultiLayerNeuralNetwork:
             self.errors.append(self.cost_function(X, y, regularization_parameter))
             print("{}'th iteration. Cost: {}".format(i + 1, self.errors[-1]))
         
-    def predict(self, X):
+    def predict(self, X, thetas = None):
         '''
         X should be a matrix of examples, with each row being one example
         '''
-        *_, h_x = self.forward_propagation(X)
+        *_, h_x = self.forward_propagation(X, thetas)
         return h_x
 
-    def gradient_approx(self):
-        pass
-
-    def gradient_checking(self):
-        pass
-            
+    def gradient_checking(self, X, y, regularization_parameter):
+        #TODO: optimize gradient checking? Maybe not neceesary
+        n = self.theta_vector.shape[0]
+        m = X.shape[0]
+        theta_approx = np.copy(self.theta_vector)
+        theta_grad_approx = np.zeros(theta_approx.size)
+        thetas = self.thete_vector_to_list(theta_approx, self.layers)
+        eps = 1e-4
+        for i in range(n):
+            print(i,'/', n)
+            eps_vec = np.zeros((n, 1))
+            eps_vec[i] = eps
+            cost = (self.cost_function(X, y, regularization_parameter, theta_approx + eps_vec, thetas) - 
+                self.cost_function(X, y, regularization_parameter, theta_approx - eps_vec, thetas)) / (2 * eps) 
+            theta_grad_approx[i] += cost
+            theta_grad_approx[i] += regularization_parameter / m * self.theta_vector[i]
+        thetas = self.thete_vector_to_list(theta_approx, self.layers)
+        theta_grad = self.theta_grad(X, y, regularization_parameter)
+        theta_grad = np.concatenate([t.flatten() for t in theta_grad])
+        return theta_grad_approx, theta_grad
 
     def save(self):
         theta_vec = np.concatenate([theta.flatten() for theta in self.thetas])
         np.savetxt("thetas.txt", theta_vec)
+
+    def make_predictions(self, X, y):
+        predictions = self.predict(X)
+        predictions = np.argmax(predictions, 1)
+        m = predictions.size
+        hits = 0
+        misses = 0
+        for t in range(m):
+            if predictions[t] == y[t]:
+                hits += 1
+            else:
+                misses += 1
+        return hits, misses